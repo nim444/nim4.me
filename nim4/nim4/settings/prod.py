9e0fec24f31962149377e506660b26d44fef0b00
nim4 <nima.karimi@live.com>
2022-01-01 05:32:07 +0300

    Update Secret

--- nim4/nim4/settings/prod.py
+++ nim4/nim4/settings/prod.py
@@ -5,21 +5,31 @@ from pathlib import Path
 config = configparser.ConfigParser()
 cdir = Path(__file__).parents[0]
 secret_file: Path = cdir / '.secret'
-config.read(secret_file)
+if secret_file.exists():
+    config.read(secret_file)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config.get('Secret', 'dbname'),
+            'USER': config.get('Secret', 'dbuser'),
+            'PASSWORD': config.get('Secret', 'dbpass'),
+            'HOST': str(config.get('Secret', 'dbip')),
+            'PORT': '5432',
+
+        }
+    }
+    SECRET_KEY = config.get('Secret', 'secret')
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+    SECRET_KEY = "Pre_Production_KEY"
 
 
-SECRET_KEY = config.get('Secret','secret')
 DEBUG = False
 ALLOWED_HOSTS = ['nim4.me','www.nim4.me',"*"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config.get('Secret', 'dbname'),
-        'USER': config.get('Secret', 'dbuser'),
-        'PASSWORD': config.get('Secret', 'dbpass'),
-        'HOST': str(config.get('Secret', 'dbip')),
-        'PORT': '5432',
 
-    }
-}
